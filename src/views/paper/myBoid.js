ac6b3158e5828ef2c92c4ed9ae9502374af4b7d7
didadida262 <719713496@qq.com>
2022-03-12 00:53:19 +0800

    home

--- src/views/paper/myBoid.js
+++ src/views/paper/myBoid.js
@@ -1,42 +1,47 @@
 import paper from 'paper'
 
-
 const Boid = paper.Base.extend({
-    initialize: function (position, maxSpeed, maxForce) {
-        var strength = Math.random() * 0.5;
-        this.acceleration = new paper.Point();
-        this.vector = paper.Point.random();
-        this.position = position.clone();
-        this.radius = 30;
-        this.maxSpeed = maxSpeed + strength;
-        this.maxForce = maxForce + strength;
-        this.amount = strength * 10 + 10;
-        this.count = 0;
-        this.createItems();
-    },
-    createItems: function() {
-        this.head = new paper.Shape.Ellipse({
-            center: [0, 0],
-            size: [13, 8],
-            fillColor: 'white'
-        });
+    initialize: function(position, maxSpeed, maxForce) {
+        console.log('执行initialize')
+        const strength = Math.random() * 0.5
+        this.acceleration = new paper.Point()
+        this.vector = paper.Point.random()
+        this.vector.x = this.vector.x * 2 - 1
+        this.vector.y = this.vector.y * 2 - 1
+        this.position = position.clone()
+        this.radius = 30
+        this.maxSpeed = maxSpeed + strength
+        this.maxForce = maxForce + strength
+        this.amount = strength * 10 + 10
+        this.count = 43366.76471028296
+        this.createItems()
+        },
 
-        this.path = new paper.Path({
-            strokeColor: 'white',
-            strokeWidth: 2,
-            strokeCap: 'round'
-        });
-        for (var i = 0; i < this.amount; i++)
-            this.path.add(new paper.Point());
+        createItems: function() {
+            console.log('执行createItems')
 
-        this.shortPath = new paper.Path({
-            strokeColor: 'white',
-            strokeWidth: 4,
-            strokeCap: 'round'
-        });
-        for (var i = 0; i < Math.min(3, this.amount); i++)
-            this.shortPath.add(new paper.Point());
-    },    
+            this.head = new paper.Shape.Ellipse({
+                center: [0, 0],
+                size: [13, 8],
+                fillColor: 'white'
+            });
+    
+            this.path = new paper.Path({
+                strokeColor: 'white',
+                strokeWidth: 2,
+                strokeCap: 'round'
+            });
+            for (var i = 0; i < this.amount; i++)
+                this.path.add(new paper.Point());
+    
+            this.shortPath = new paper.Path({
+                strokeColor: 'white',
+                strokeWidth: 4,
+                strokeCap: 'round'
+            });
+            for (var i = 0; i < Math.min(3, this.amount); i++)
+                this.shortPath.add(new paper.Point());
+        },
 })
 
 
