6b4982a6bed8c8087f20928125e12c4492b574ca
hhvcg <719713496@qq.com>
2022-05-07 13:43:00 +0800

    高效代码测试

--- test.js
+++ test.js
@@ -1,19 +1,26 @@
-const Pet = function(name, year) {
-  const that = {
-    name: name,
-    year: year,
-    say: function() {
-      console.log(`i am ${that.name},and ${that.year} old years`)
-    }
+/*
+ * @Author: Hhvcg
+ * @Date: 2022-04-15 18:09:58
+ * @LastEditors: -_-
+ * @Description: 
+ */
+// 斐波那契数列
+
+
+// 测试程序运行时间
+// n: 40
+// 平方级别的时间消耗都已经如此夸张，指数级别的程序，可想而知。各位骚年，少些狗屎代码祸害机器了
+const F = (n) => {
+  const res = [0, 1]
+  for (let i = 2; i < n; i++) {
+    res[i] = res[i -1] + res[i-2]
+    // for (let j = 0; j < i; j++) {
+    //   console.log('')
+    // }
   }
-  return that
+  console.log('所有数据--->', res)
 }
 
-const Dog = function(name, year) {
-  const that = Pet(name, year)
-  return that
-}
-const dog = Dog('kaka', 2)
-dog.name = 'nima'
-console.log(dog)
-dog.say()
+console.time()
+F(400)
+console.timeEnd()
