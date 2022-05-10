b46d5a4a409809646948adb156345e1ff8d265b3
hhvcg <719713496@qq.com>
2022-05-10 23:18:59 +0800

    新增炮塔移动

--- src/views/paper/Tank/index.vue
+++ src/views/paper/Tank/index.vue
@@ -12,6 +12,10 @@ export default {
 
   data() {
     return {
+      GUNWIDTH: 50,
+      GUNHEIGHT: 100,
+      BOTTOMWIDTH: 300,
+      BOTTOMHEIGHT: 100,
       // 炮弹库
       bombs: [],
       paper: null,
@@ -34,6 +38,10 @@ export default {
   },
 
   methods: {
+    updateBottom(point) {
+      this.role.bottom.position = this.role.bottom.position.add(point).clone()
+      this.role.gun.position = this.role.gun.position.add(point).clone()
+    },
     init() {
       const canvas = this.$refs.tank;
       // canvas的dom节点给到paper装载
@@ -42,26 +50,40 @@ export default {
       this.paper.view.onFrame = this.onFrame
       this.tool = new this.paper.Tool()
       this.tool.onKeyDown = (e) => {
-        if (e.key === 'space') {
-          // 发射
-          this.bombs.push(new Bomb(new this.paper.Point(this.role.gun.position.x, this.role.gun.position.y - 55), 10))
-          console.log('this.bombs',this.bombs)
+        switch(e.key) {
+          // fire！！！
+          case 'space':
+            this.bombs.push(new Bomb(new this.paper.Point(this.role.gun.position.x, this.role.gun.position.y - this.GUNHEIGHT / 2 - 5), 10))
+            break
+          case 'left':
+            console.log('left')
+            this.updateBottom(new paper.Point(-8, 0))
+            break
+          case 'right':
+            console.log('right')
+            this.updateBottom(new paper.Point(8, 0))
+            break
+          case 'up':
+            console.log('up')
+            break
+          case 'down':
+            console.log('down')
+            break
         }
-      }
-      console.log('this.tool:', this.tool)
-      console.log('初始化坦克世界')
-      console.log('canvas',canvas)
 
+        // if (e.key === 'space') {
+        //   // 发射
+        //   this.bombs.push(new Bomb(new this.paper.Point(this.role.gun.position.x, this.role.gun.position.y - this.GUNHEIGHT / 2 - 5), 10))
+        // }
+      }
+      console.log('初始化坦克世界')
       console.log(this.paper)
-      this.role.bottom = new this.paper.Shape.Rectangle(this.paper.view.size.width / 2 - 150, this.paper.view.size.height - 100, 300, 100)
+      this.role.bottom = new this.paper.Path.Rectangle(this.paper.view.size.width / 2 - this.BOTTOMWIDTH / 2, this.paper.view.size.height - this.BOTTOMHEIGHT, this.BOTTOMWIDTH, this.BOTTOMHEIGHT)
       this.role.bottom.fillColor = 'green'
-      this.role.gun = new this.paper.Shape.Rectangle(this.paper.view.size.width / 2 - 25, this.paper.view.size.height - 200, 50, 100)
+      this.role.gun = new this.paper.Path.Rectangle(this.role.bottom.position.x - this.GUNWIDTH / 2, this.role.bottom.position.y - this.GUNHEIGHT - this.BOTTOMHEIGHT / 2, this.GUNWIDTH, this.GUNHEIGHT)
       this.role.gun.fillColor = 'red'
-      console.log('this.role.gun:', this.role.gun)
-    
     },
     onFrame () {
-      console.log('震动')
       for (let bomb of this.bombs) {
         bomb.update()
       }
