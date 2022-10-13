a59d88edd0cbd4905e2f36b746335df69e34e35e
hhvcg <719713496@qq.com>
2022-10-13 20:07:48 +0800

    m: airplane

--- src/views/paper/Airplane/index.vue
+++ src/views/paper/Airplane/index.vue
@@ -18,7 +18,7 @@ import paper from "paper";
 import commonTemplate from '@/components/titleTemplate.vue'
 import { getRandomColor } from '@/weapons'
 import tool from '@/components/tool'
-import { Ariplane } from './myAriplane'
+// import { Ariplane } from './myAriplane'
 export default {
   name: 'Airplane',
   mixins: [tool],
@@ -38,10 +38,8 @@ export default {
   },
   methods: {
     onFrame() {
-
     },
     fire() {
-      
     },
     onKeyDown(e) {
       switch (e.key) {
@@ -50,11 +48,9 @@ export default {
           break;
         case 'left':
           this.Ship.rotate(-10)
-          console.log(this.Ship)
           break;
         case 'right':
           this.Ship.rotate(10)
-          console.log(this.Ship)
           break;
         case 'up':
           this.Ship.position = this.Ship.position.subtract(new paper.Point(0, 5)).clone()
@@ -80,19 +76,16 @@ export default {
       this.tool = new paper.Tool()
       this.tool.onMouseDown = (e) => {
         // console.log('tool的鼠标点击事件---->', e)
-        const res = this.myPath.contains(e.point)
-        console.log('res--->', res)
+        // const res = this.myPath.contains(e.point)
       }
       console.log("初始化世界!!!");
-      this.ari = new Ariplane()
-      this.myPath = new paper.Path.Rectangle(new paper.Point(-400,-400), new paper.Size(100))
-      this.myPath.strokeColor = getRandomColor()
-      
-
-
-      // console.log(this.Ship)
-      // this.showOperations()
-      
+      // this.ari = new Ariplane()
+      // this.myPath = new paper.Path.Rectangle(new paper.Point(-400,-400), new paper.Size(100))
+      // this.myPath.strokeColor = getRandomColor()
+      this.Ship = new paper.Path([0, 0], [20, 20],[16, 22], [11, 15], [8, 16], [4, 12], [0, 20], [-4, 12], [-8, 16], [-11, 15], [-16, 22], [-20, 20])
+      this.Ship.strokeColor = getRandomColor()
+      this.Ship.closed = true
+      console.log('paperScope---', this.paper)
     },
   },
   beforeDestroyed() {
@@ -107,12 +100,10 @@ export default {
 .container {
   width: 100%;
   height: 100%;
-
   .Airplane-container-content {
     width: 100%;
     height: calc(100% - 80px);
     border: 1px solid rgb(118, 118, 122, 0.5);
-
     .canvas {
       width: 100%;
       height: 100%;
