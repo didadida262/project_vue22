e021898735d2936162337094b20613342108726a
hhvcg <719713496@qq.com>
2022-10-31 16:09:23 +0800

    add: commit规范

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: Hhvcg
+ * @Date: 2022-10-31 16:08:35
+ * @LastEditors: -_-
+ * @Description:
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'add', 'fix', 'refactor', 'docs', 'style', 'chore', 'scope'
+    ]]
+  }
+}
