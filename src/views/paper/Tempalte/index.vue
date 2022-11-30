ba3f688ad1860e9a821878eb43df45e3a1db1d42
hhvcg <719713496@qq.com>
2022-11-30 16:45:00 +0800

    add: 新增template

---
+++ src/views/paper/Tempalte/index.vue
@@ -0,0 +1,56 @@
+<!--
+ * @Author: Hhvcg
+ * @Date: 2022-11-30 16:42:33
+ * @LastEditors: -_-
+ * @Description: records记录，存储后端json
+-->
+
+<template>
+  <div class="Records-container pd10">
+    <commonTemplate title="Records" />
+    <div class="Records-container-content">
+      <canvas ref="canvas" resize class="canvas" />
+    </div>
+  </div>
+</template>
+
+<script>
+import commonTemplate from '@/components/titleTemplate.vue'
+
+export default {
+  name: 'Records',
+  components: {
+    commonTemplate
+  },
+  data() {
+    return {
+      title: 'Records',
+      WIDTH: null,
+      HEIGHT: null
+    }
+  },
+  computed: {
+  },
+  watch: {},
+  mounted() {
+
+  },
+  beforeDestroy() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+<style scoped lang="scss">
+.Records-container {
+  width: 100%;
+  height: 100%;
+  &-content {
+    width: 100%;
+    height: calc(100% - 80px);
+    border: 1px solid rgb(118, 118, 122, 0.5);
+  }
+}
+</style>
