57aacfa8aebbbf7e52f48fce724a683577e33e6a
hhvcg <719713496@qq.com>
2023-01-02 12:06:42 +0800

    add: 局域网共享

--- src/utils/request.js
+++ src/utils/request.js
@@ -12,7 +12,7 @@ import { getToken } from '@/utils/auth'
 // create an axios instance
 const service = axios.create({
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://192.168.0.106:3000',
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 50000 // request timeout
 })
