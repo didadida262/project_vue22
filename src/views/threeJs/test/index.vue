77ad880b322a4bd3753fd46367253ab302ad5cf8
hhvcg <719713496@qq.com>
2023-02-23 15:37:42 +0800

    add: renhua

--- src/views/threeJs/test/index.vue
+++ src/views/threeJs/test/index.vue
@@ -22,10 +22,22 @@ export default {
   },
   mounted() {
     this.initWorld()
+    this.animate()
   },
 
   methods: {
-
+    animate() {
+      console.log('执行>>>>')
+      requestAnimationFrame(this.animate)
+      // mesh.rotation.x += 0.01
+      // mesh.rotation.y += 0.02
+      // const elapsedTime = clock.getElapsedTime()
+      // Update objects
+      // this.mesh.rotation.y = 0.5 * elapsedTime
+      this.mesh.rotation.x += 0.01
+      this.mesh.rotation.y += 0.01
+      this.renderer.render(this.scene, this.camera)
+    },
     initWorld() {
       // threejs三要素：相机、场景、渲染器
       console.log('初始化世界---->')
@@ -33,15 +45,15 @@ export default {
       this.containerWidth = container.clientWidth // 窗口宽度
       this.containerHeight = container.clientHeight // 窗口高度
       this.scene = new Three.Scene()
-      const geometry = new Three.SphereBufferGeometry(60, 40, 40)
-      const material = new Three.MeshStandardMaterial({
+      this.geometry = new Three.SphereBufferGeometry(60, 40, 40)
+      this.material = new Three.MeshStandardMaterial({
         // color: 'black'
       })
-      material.metalness = 0.7
-      material.roughness = 0.2
-      material.color = new Three.Color(0x292929)
-      const mesh = new Three.Mesh(geometry, material)
-      this.scene.add(mesh)
+      this.material.metalness = 0.7
+      this.material.roughness = 0.2
+      this.material.color = new Three.Color(0x292929)
+      this.mesh = new Three.Mesh(this.geometry, this.material)
+      this.scene.add(this.mesh)
 
       // 光源设置
       // 点光
@@ -59,18 +71,18 @@ export default {
       const k = width / height // 窗口宽高比
       const s = 200 // 三维场景显示范围控制系数，系数越大，显示的范围越大
       // 创建相机对象
-      const camera = new Three.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000)
-      camera.position.set(200, 300, 200) // 设置相机位置
-      camera.lookAt(this.scene.position) // 设置相机方向(指向的场景对象)
+      this.camera = new Three.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000)
+      this.camera.position.set(200, 300, 200) // 设置相机位置
+      this.camera.lookAt(this.scene.position) // 设置相机方向(指向的场景对象)
       /**
        * 创建渲染器对象
        */
-      const renderer = new Three.WebGLRenderer()
-      renderer.setSize(width, height)// 设置渲染区域尺寸
-      renderer.setClearColor(0x401230, 0.5) // 设置背景颜色
-      container.appendChild(renderer.domElement) // body元素中插入canvas对象
+      this.renderer = new Three.WebGLRenderer()
+      this.renderer.setSize(width, height)// 设置渲染区域尺寸
+      this.renderer.setClearColor(0x401230, 1) // 设置背景颜色
+      container.appendChild(this.renderer.domElement) // body元素中插入canvas对象
       // 执行渲染操作   指定场景、相机作为参数
-      renderer.render(this.scene, camera)
+      this.renderer.render(this.scene, this.camera)
     }
   }
 }
