4972ebe01d9ee0b7e4051041a37fffcf45f678b0
hhvcg <719713496@qq.com>
2023-03-06 11:30:10 +0800

    add: running-way1.0

---
+++ src/views/paper/RunningWay/components/PaperView.vue
@@ -0,0 +1,108 @@
+<!--
+ * @Author: Hhvcg
+ * @Date: 2022-09-16 11:38:49
+ * @LastEditors: -_-
+ * @Description: 支持图片展示、拖拽、放大缩小功能
+-->
+
+<template>
+  <canvas
+    :id="picContainer"
+    :ref="picContainer"
+    resize
+    class="picContainer"
+  />
+</template>
+<script>
+import paper from 'paper'
+export default {
+  name: 'paperpicsample',
+  props: {
+    picInfo: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      zoom: 1,
+      WIDTH: null,
+      HEIGHT: null,
+      // 拖动的初始点
+      initPoint: null
+    }
+  },
+  methods: {
+    changeZoom(delta, p) {
+      const view = this.currentProject.view
+      const oldZoom = view.zoom
+      const c = view.center
+      const factor = 0.05 + this.zoom
+
+      const zoom = delta < 0 ? oldZoom * factor : oldZoom / factor
+      const beta = oldZoom / zoom
+      // 计算当前点到当前视图中心点向量指向
+      const pc = p.subtract(c)
+      // a点目测是换算后的新p点
+      const a = p.subtract(pc.multiply(beta)).subtract(c)
+
+      return { zoom: zoom, offset: a }
+    },
+    // 初始化画布，并确认相关参数初始值
+    init() {
+      const canvas = this.$refs[this.picContainer]
+      this.WIDTH = canvas.clientWidth
+      this.HEIGHT = canvas.clientHeight
+      paper.setup(canvas)
+      this.paper = paper
+      this.paper.project.name = this.picContainer
+      this.paper.view.setCenter(0, 0)
+      this.paper.view.onMouseDown = this.onMouseDown
+      this.paper.view.onMouseDrag = this.onMouseDrag
+      this.paper.view.onMouseMove = this.onMouseMove
+      this.paper.view.onMouseUp = this.onMouseUp
+    },
+    onMouseDown(e) {
+      this.currentProject.activate()
+      this.$emit('handleChangePaperScope', this.picInfo)
+    },
+    onMouseDrag(e) {
+    },
+    onMouseMove(e) {
+    },
+    onMouseUp(e) {
+    },
+
+    drawPic() {
+      const raster = new paper.Raster(this.picInfo.src)
+      raster.onLoad = () => {
+        raster.fitBounds(this.paper.view.bounds, false)
+      }
+    }
+  },
+  computed: {
+    picContainer() {
+      return 'picContainer' + this.picInfo.title
+    },
+    currentProject() {
+      return this.paper.projects.filter((item) => item.name === this.picContainer)[0]
+    }
+  },
+  mounted() {
+    this.init()
+    this.drawPic()
+  },
+  beforeDestroy() {
+    const currentProject = this.paper.projects.filter((p) => p.name === this.picContainer)
+    currentProject.remove()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.picContainer {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
--- src/views/paper/RunningWay/index.vue
+++ src/views/paper/RunningWay/index.vue
@@ -2,88 +2,73 @@
  * @Author: Hhvcg
  * @Date: 2023-02-22 17:39:55
  * @LastEditors: -_-
- * @Description:
+ * @Description: runnigway--app
 -->
 
 <template>
   <div class="runningWay-container pd10">
     <commonTemplate title="Running-Way" />
     <div class="runningWay-container-content">
-      <canvas ref="canvas" resize class="canvas" />
+      <paper-view
+        class="picview-item-st"
+        v-for="(pic, index) in picviewlist"
+        :key="index"
+        :picInfo="pic"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import paper from 'paper'
 import commonTemplate from '@/components/titleTemplate.vue'
-import { getRandomColor } from '@/utils/weapons'
-import tools from './tools'
+import PaperView from './components/PaperView.vue'
 
 export default {
-  mixins: [tools],
   name: 'runningWay',
   components: {
-    commonTemplate
+    commonTemplate,
+    PaperView
   },
   data() {
     return {
       title: 'runningWay',
-      WIDTH: null,
-      HEIGHT: null
+      picviewlist: [
+        {
+          title: 'pl',
+          src: require('@/assets/Sam.webp')
+        },
+        {
+          title: 'pl',
+          src: require('@/assets/Sam.webp')
+        },
+        {
+          title: 'pl',
+          src: require('@/assets/Sam.webp')
+        },
+        {
+          title: 'pl',
+          src: require('@/assets/Sam.webp')
+        },
+        {
+          title: 'pl',
+          src: require('@/assets/Sam.webp')
+        },
+        {
+          title: 'pl',
+          src: require('@/assets/Sam.webp')
+        }
+      ]
     }
   },
   computed: {
-    currentProject() {
-      return this.paper.projects.filter((_p) => _p.name === this.title)[0]
-    }
+
   },
   watch: {},
   mounted() {
-    this.init()
-    this.draw()
   },
   beforeDestroy() {
-    const currentProject = this.paper.projects.filter((_p) => _p.name === this.title)[0]
-    if (currentProject) {
-      currentProject.remove()
-    }
-    if (this.paper) {
-      this.paper = null
-    }
   },
   methods: {
-    random() {
-      return paper.Point.random().multiply(this.WIDTH, this.HEIGHT)
-    },
-    getRandomPoint() {
-      return new paper.Point(Math.random() * this.WIDTH, Math.random() * this.HEIGHT)
-    },
-    draw() {
-    },
-    onFrame() {
-    },
-    onMouseDown(e) {
-    },
-    onMouseDrag(e) {
-    },
-    onMouseMove(e) {
-    },
-    onMouseUp(e) {
-    },
-    onKeyDown(e) {
-    },
-    init() {
-      const canvas = this.$refs.canvas
-      this.WIDTH = canvas.clientWidth
-      this.HEIGHT = canvas.clientHeight
-      this.paper = paper
-      this.paper.setup(canvas)
-      this.paper.project.name = this.title
-      this.paper.view.onFrame = this.onFrame
-      this.paper.view.onMouseDown = this.onMouseDown
-      console.log('初始化世界!!!')
-    }
   }
 }
 </script>
@@ -95,10 +80,10 @@ export default {
     width: 100%;
     height: calc(100% - 80px);
     border: 1px solid rgb(118, 118, 122, 0.5);
-    .canvas {
-      width: 100%;
-      height: 100%;
-      background: black;
+    .picview-item-st {
+      width: 30%;
+      height: 200px;
+      border: 1px solid red;
     }
   }
 }
