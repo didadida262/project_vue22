f10cce15b71ec52ffb1d470e3f70cf1cf75a9a45
hhvcg <719713496@qq.com>
2023-04-24 18:07:49 +0800

    add: test_speed

--- test.js
+++ test.js
@@ -1,34 +1,17 @@
-/*
- * @Author: Hhvcg
- * @Date: 2023-01-30 11:05:10
- * @LastEditors: -_-
- * @Description:
- */
-
-const test = {
-  name: 'hhvcg',
-  old: 10
+const arr = []
+for (let i = 0; i < 2000000; i++) {
+  const obj = {
+    index: i,
+    val: Math.random() * 100000
+  }
+  arr.push(obj)
 }
-
-// const p = new Promise((resolve, reject) => {
-//   // rejected()
-//   // resolve()
-//   reject()
-// })
-
-// p.then(() => {
-//   console.log('resolve')
-// })
-// p.catch(() => {
-//   console.log('reject')
-// })
-// console.log('p>>', p)
-
-const arr = new Array(1000).fill(0)
 
 console.time('time')
-for (let i = 0; i < arr.length; i++) {
-  console.log('i>>', i)
-}
+arr.sort((a, b) => {
+  return b.val - a.val
+})
 console.timeEnd('time')
+
+console.log(arr)
 
